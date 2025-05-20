461e5062cedcd4e35ab11d000de1ed89901e1ca0
Axegass <162570044+Axegass@users.noreply.github.com>
2025-05-20 22:23:05 +0700

    tugas_pertemuan_11

---
+++ resources/views/page.blade.php
@@ -0,0 +1,48 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+  <title>Blasterjaxx</title>
+  <link href="https://fonts.googleapis.com/css2?family=Inter:wght@400;600;700&display=swap" rel="stylesheet"/>
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.0/css/all.min.css"/>
+  <link rel="stylesheet" href="blasterjaxx.css"/>
+</head>
+<body>
+    <br>
+  <div class="top-bar">
+    <button class="bell-btn"><img src="bell_blank.png" alt="Notifications" /></button>
+    <button class="icon-btn"><i class="fas fa-ellipsis-h"></i></button>
+  </div>
+
+  <img class="profile-img" src="images/background-home-rev-2025.jpg" alt="Profile Picture"/>
+  <h1 class="username">My PWEB Project</h1>
+
+  <a class="link-btn" href="/blog"><img src="images/blog.png" alt=""/> <div class="link-content"> <span class="link-text">Blog</span> </div> <button class="dots-btn" >⋮</button></a>
+  <a class="link-btn" href="/css_background&masking.png"><img src="images/masking&background.png" alt=""/> <span class="link-text">Background & Masking</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/JS1"><img src="images/JS1.png" alt=""/>  <div class="link-content"> <span class="link-text">JS1</span> </div> <button class="dots-btn">⋮</button>
+  <a class="link-btn" href="/JS2"><img src="images/JS2.png" alt=""/> <div class="link-content"> <span class="link-text">JS2</span></div> <button class="dots-btn">⋮</button> </a>
+  <a class="link-btn" href="/bootstrap1"><img src="images/bootstrap1.png" alt=""/> <span class="link-text">Bootstrap1</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/pertama"><img src="images/pertama.png" alt=""/> <span class="link text">Pertama</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/sidebar"><img src="images/sidebar.png" alt=""/> <span class="link text">Sidebar</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/welcome"><img src="images/Welcome.png" alt=""/> <span class="link text">Welcome</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/index"><img src="images/Index.png" alt=""/> <span class="link text">Index (UTS)</span> <button class="dots-btn" type="button">⋮</button></a>
+  <a class="link-btn" href="/blasterjaxx"><img src="images/Blasterjax.png" alt=""/> <span class="link text">Blasterjaxx (LinkTree)</span> <button class="dots-btn" type="button">⋮</button></a>
+
+
+
+  <div class="social-icons">
+    <a href="https://www.facebook.com/BlasterjaxxOfficial"><img src="faceboook_blank.png" alt="facebook" /></a>
+    <a href="https://instagram.com/Blasterjaxx"><img src="Instagram_blank.png" alt="Instagram" /></a>
+    <a href="https://tiktok.com/@blasterjaxx_official"><img src="tiktok_blank.png" alt="Tiktok" /></a>
+    <a href="https://open.spotify.com/artist/37awA8DFCAnCCL7aqYbDnD"><img src="Spotify_blank.png" alt="Spotify" /></a>
+    <a href="https://www.youtube.com/channel/UC3hV4vnwzTUPSb7CAx4mXvg"><img src="youtube_blank.png" alt="Youtube" /></a>
+    <a href="https://soundcloud.com/blasterjaxx"><img src="soundcloud_blank.png" alt="Soundcloud" /></a>
+    <a href="https://x.com/Blasterjaxx"><img src="X_blank.png" alt="X" /></a>
+
+  </div>
+
+
+</body>
+
+</html>
--- routes/web.php
+++ routes/web.php
@@ -7,23 +7,61 @@ use App\Http\Controllers\Link ;
 
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('page');
 });
 
-Route::get('halo', function () {
+Route::get('/halo', function () {
 	return "<h2>Halo, Selamat datang di tutorial laravel www.malasngoding.com</h2>";
 });
 
-Route::get('blog', function () {
-	return view('blog');
-});
+// Route::get('/blog', function () {
+// 	return view('blog');
+// });
 
 
-Route::get('hello', [Link::class, 'helloword'])
+//Route::get('hello', [Link::class, 'helloword'])
 
 
 // ================Tugas 1=========================
 
-Route::get('pertama', function () {
+Route::get('/pertama', function () {
 	return view('pertama');
 });
+
+Route::get('/blasterjaxx', function () {
+	return view('blasterjaxx');
+});
+
+Route::get('/blog', function () {
+	return view('blog');
+});
+
+Route::get('/bootstrap1', function () {
+	return view('bootstrap1');
+});
+
+Route::get('/css_background&masking', function () {
+	return view('css_background&masking');
+});
+
+Route::get('/index', function () {
+	return view('index');
+});
+
+Route::get('/JS1', function () {
+	return view('JS1');
+});
+
+Route::get('/JS2', function () {
+	return view('JS2');
+});
+
+Route::get('/sidebar', function () {
+	return view('sidebar');
+});
+
+Route::get('/welcome', function () {
+	return view('welcome');
+});
+
+?>
