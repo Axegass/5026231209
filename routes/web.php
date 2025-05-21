d60fdac1212e9d4e307dbc2b87a1e4f7c15f5e30
Axegass <162570044+Axegass@users.noreply.github.com>
2025-05-21 12:35:36 +0700

    Pertemuan 12

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Link ;
+use App\Http\Controllers\PegawaiController ;
+use App\Http\Controllers\BlogController ;
 
 
+//import java.io
 
 
 Route::get('/', function () {
@@ -64,4 +67,22 @@ Route::get('/welcome', function () {
 	return view('welcome');
 });
 
+//==========================Pertemuan 12=========================================
+//route dosen
+Route::get('dosen', [Link::class, 'index']);
+//route pegawai
+Route::get('/pegawai/{nama}', [PegawaiController::class, 'index']);
+
+//route formulir
+Route::get('/formulir', [PegawaiController::class, 'formulir']);
+Route::post('/formulir/proses', [PegawaiController::class, 'proses']);
+
+// route blog
+Route::get('/blog', [BlogController::class, 'home']);
+Route::get('/blog/tentang', [BlogController::class, 'tentang']);
+Route::get('/blog/kontak', [BlogController::class, 'kontak']);
+
+
+
 ?>
+
